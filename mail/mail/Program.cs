fed84baf7ab44b6b585c8df57aded162c10c0a6c
Salieri0009 <maxman_07@bk.ru>
2019-02-27 19:54:21 +0300

    дабавил протокол imap для скачивания почты

--- mail/mail/Program.cs
+++ mail/mail/Program.cs
@@ -11,24 +11,33 @@ using OpenPop.Mime.Header;
 using OpenPop.Pop3;
 using OpenPop.Pop3.Exceptions;
 using OpenPop.Common.Logging;
-
+using MailKit;
+using MailKit.Net.Imap;
 
 namespace mail
 {
     class Mail
     {
-        private Pop3Client Client = new OpenPop.Pop3.Pop3Client();
+        private Pop3Client Client_pop3 = new OpenPop.Pop3.Pop3Client();
+        ImapClient Client_imap = new ImapClient();
         private Dictionary<int, Message> Messages = new Dictionary<int, Message>();
-        private string Add, Password, Pop ,Smtp;
+        private string Add, Password, Pop ,Smtp, Imap;
         public Mail(){Add="пусто";}
 
         void Set_add()
         {
-            Console.WriteLine("Введите адрес электроной почты");
+            Console.WriteLine("Введите imap");
             string add = Console.ReadLine();
-            Add = add;
+            Add= add;
         }
         string Get_add() { return Add; }
+
+        void Set_imap()
+        {
+            Console.WriteLine("введите pop");
+            var imap = Console.ReadLine();
+            Imap = imap;
+        }
 
         void Set_pop()
         {
@@ -61,14 +70,13 @@ namespace mail
             Console.WriteLine();
             Password = inpt;
         }
-        string Get_pass() { return Password; }
 
         void Conection_pop3()
         {
             try {
-                Client.Connect(Pop, 995, true);
-                Client.Authenticate(Add, Password, AuthenticationMethod.UsernameAndPassword);
-                if (Client.Connected)
+                Client_pop3.Connect(Pop, 995, true);
+                Client_pop3.Authenticate(Add, Password, AuthenticationMethod.UsernameAndPassword);
+                if (Client_pop3.Connected)
                 {
                     Console.WriteLine("вы подключены");
                 }
@@ -84,19 +92,19 @@ namespace mail
             try
             {
                 MailMessage Message = new MailMessage();
-                Message.From = new MailAddress(Add);
+                Message.From = new System.Net.Mail.MailAddress(Add);
                 Console.WriteLine("куда хотите отправить сообщение");
                 var add2 = Console.ReadLine();
-                Message.To.Add(new MailAddress(add2));
+                Message.To.Add(new System.Net.Mail.MailAddress(add2));
                 Console.WriteLine("введите тему сообщения");
                 var teme = Console.ReadLine();
                 Console.WriteLine("введите тему сообщения");
                 var masseg = Console.ReadLine();
                 Message.Subject = teme;
                 Message.Body = "сообщение";
-                SmtpClient client = new SmtpClient("smtp.mail.ru", 25);
+                SmtpClient client = new SmtpClient(Smtp, 25);
                 client.Port = 587;
-                client.Credentials = new System.Net.NetworkCredential(Add, Password);
+                client.Credentials = new NetworkCredential(Add, Password);
                 client.EnableSsl = true;
                 try
                 {
@@ -118,11 +126,11 @@ namespace mail
             try
             {
                 Conection_pop3();
-                int count = Client.GetMessageCount();
+                int count = Client_pop3.GetMessageCount();
                 for (int i = count; i >= 1; i--)
                 {
                     string body;
-                    Message message = Client.GetMessage(i);
+                    Message message = Client_pop3.GetMessage(i);
                     MessagePart plainTextPart = message.FindFirstPlainTextVersion();
                     if (plainTextPart != null)
                     {
@@ -154,13 +162,33 @@ namespace mail
             }
         }
 
+        void Take_mes_imap()
+        {
+            Client_imap.Connect(Imap,993,true);
+            Client_imap.Authenticate(Add,Password);
+            IMailFolder inbox = Client_imap.Inbox;
+            inbox.Open(FolderAccess.ReadOnly);
+            Console.WriteLine("Входящие {0}", inbox.Count);
+            Console.WriteLine("введите номер сообщения которое вы хотите получить");
+            try
+            {
+                var i = Convert.ToInt32(Console.ReadLine());
+                var massage = inbox.GetMessage(i);
+                Console.WriteLine("Subject: {0}", massage.TextBody);
+            }
+            catch
+            {
+                Console.WriteLine("неправельно введены данные");
+            }
+        }
+
         public void Init()
         {
             string i;
             while(true)
             {
                 Console.WriteLine("Элементы управления"+ "\r\n"+ "1)ввести логин" + "\r\n" + " 2)ввести пароль" + "\r\n" + " 3)ввести pop" + "\r\n" + " 4)ввести smtp" + "\r\n"  +
-                    " 5)получить сообщения" + "\r\n" + " 6)отправить сообщение" + "\r\n" + " 7)выход");
+                    " 5)ввести imap" + "\r\n" + " 6)получить сообщения pop3" + "\r\n" + " 7)получить сообщения imap" + "\r\n"+ " 8)отправить сообщение" + "\r\n" + " 9)выход");
                 i = Console.ReadLine();
                 if (i=="1")
                 {
@@ -180,13 +208,21 @@ namespace mail
                 }
                 if (i == "5")
                 {
-                    Take_mes_pop3();
+                    Set_imap();
                 }
                 if (i == "6")
                 {
-                    Send_mes_smtp();
+                    Take_mes_pop3();
                 }
                 if (i == "7")
+                {
+                    Take_mes_imap();
+                }
+                if (i == "8")
+                {
+                    Send_mes_smtp();
+                }
+                if (i == "9")
                 {
                     break;
                 }
